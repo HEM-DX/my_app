8e991b5357e6773558dd9330561aa835c1d26fe3
sawada ryutaro <ryutaro.sawada134011@gmail.com>
2025-07-08 16:57:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,6 +112,7 @@ try:
 
 if st.button("✅ 確定してExcelに保存"):
     try:
+        # ここに処理を書く（省略せずに全部書く）
         template_path = r"C:\Users\J0134011\OneDrive - Honda\デスクトップ\my_app\my_streamlit_app\calendar_template.xlsx"
         wb = load_workbook(template_path)
         ws = wb.active
